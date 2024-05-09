170d63881289641d94890ecbed7698c21f14080f
Rafael222-R <rafael_dias18@hotmail.com>
2024-05-08 22:19:19 -0300

    ajustando pasta , aula 10 e 11

--- 10-mongodb/src/index.js
+++ 10-mongodb/src/index.js
@@ -1,4 +1,24 @@
- const express = require('require');
- const app = express()
 
- 
+// Middlewares
+app.use(express.json())
+
+// Conexão com o MongoDB
+mongoose.connect(`mongodb+srv://username:<password>@cluster0.g6opllz.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => console.log("Conectado ao MongoDB"))
+    .catch(err => console.log("Erro ao conectar no MongoDB: ", err))
+
+
+// Schemas
+const Tarefa = mongoose.model('tarefa', { nome: String })
+
+// Rotas
+app.post('/tarefas', async (req, res) => {
+    const tarefa = new Tarefa({ nome: req.body.nome })
+    const tarefaCriada = await tarefa.save()
+    res.json(tarefaCriada)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Aplicação rodando na porta ${PORT}`)
+})
